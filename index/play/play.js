4b9bc629d80efa7924bf719f2e9254a1dc4c86f1
Guohjia <guohjia@gmail.com>
2017-09-05 00:23:54 +0800

    lyric srcoll

--- index/play/play.js
+++ index/play/play.js
@@ -11,7 +11,7 @@
 
     function getlyric(lyric){
         let array = lyric.split('\n')
-        let reg = /^(\[.+\])(.*)$/;
+        let reg = /^\[(.+)\](.*)$/;
         let $lyric=$('.lyric')
         array = array.map(function (item) {
             let result = reg.exec(item)
@@ -44,6 +44,33 @@
             audio.pause()
             $('.disc').removeClass('playing')
         })
+        setInterval(function(){
+            let seconds=audio.currentTime
+            let minutes=~~(seconds/60)
+            let leftTime=seconds-minutes*60
+            let time=`${pad(minutes)}:${pad(leftTime)}`
+            let $p=$('.lyric>p')
+            let $matchLyric
+            for(var i=0;i<$p.length;i++){              
+                if($p.eq(i).length!==0&&$p.eq(i).attr('data-time') < time&&time<$p.eq(i+1).attr('data-time')){//如果时间再两条歌词显示时间之间,显示前一条歌词,且前提是有下一条歌词
+                    $matchLyric=$p.eq(i)
+                    break;
+                }   
+            }
+            if($matchLyric){
+                let top=$matchLyric.offset().top
+                console.log(top)
+                let lineTop=$('.lyric').offset().top
+                console.log(lineTop)
+                let delta=top - lineTop -$('.lyric-wrapper').height()/3
+                $('.lyric').css('transform',`translateY(-${delta}px)`)
+                $matchLyric.addClass('active').prev().removeClass()
+            }
+        },500)  //500ms匹配依次
+    }
+
+    function pad(number){
+        return number>=10? number+ '':'0'+number
     }
     // $.get('./lyric.json').then(function (object) {
     //     let { lyric } = object
