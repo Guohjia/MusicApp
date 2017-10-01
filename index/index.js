d6edb67963d06e1d70f052854110a0e4dcaf6d71
Guohjia <guohjia@gmail.com>
2017-10-01 23:13:23 +0800

    完善安卓兼容性

--- index/index.js
+++ index/index.js
@@ -1,18 +1,18 @@
 $(function () {
     //主页请求
     $.get('./song.json').then(function (response) {
-        let item = response
-        item.forEach((i) => {
-            let $li='<li>'+
-                 '<h3>'+i.name+'<span>'+i.description+'</span></h3>'+
-                 '<p>'+
-                     '<svg t="1504499575221" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2412" xmlns:xlink="http://www.w3.org/1999/xlink" width="4.2vw" height="6vw"><defs></defs><path d="M537.456788 684.682921l198.722994 0 18.48398 18.023492c5.709025 5.565762 13.102413 8.336876 20.490683 8.336876 7.636934 0 15.266705-2.962471 21.018709-8.859785 11.317767-11.607362 11.083429-30.191626-0.522909-41.509393l-17.499559-17.063631L778.150686 373.540532c0-16.210193-13.143345-29.352515-29.353538-29.352515L537.456788 344.188017c-16.210193 0-29.352515 13.143345-29.352515 29.352515l0 281.788851C508.104273 671.539576 521.246595 684.682921 537.456788 684.682921zM566.810327 402.893047l152.634306 0L719.444633 586.367755l-2.808976-2.739391c-11.611455-11.317767-30.193673-11.081383-41.509393 0.522909-11.317767 11.607362-11.083429 30.191626 0.522909 41.509393l0.323365 0.315178L566.810327 625.975844 566.810327 402.893047z" p-id="2413" fill="#FE672E"></path><path d="M220.442668 625.976868c-16.210193 0-29.352515 13.143345-29.352515 29.353538s13.143345 29.352515 29.352515 29.352515l211.342406 0c16.210193 0 29.352515-13.143345 29.352515-29.352515L461.137589 514.433422c0-16.210193-13.143345-29.353538-29.352515-29.353538L249.796206 485.079884l0-82.187861 181.989891 0c16.210193 0 29.352515-13.143345 29.352515-29.352515 0-16.210193-13.143345-29.352515-29.352515-29.352515L220.442668 344.186993c-16.210193 0-29.352515 13.143345-29.352515 29.352515l0 140.893914c0 16.210193 13.143345 29.352515 29.352515 29.352515l181.989891 0 0 82.189907L220.442668 625.975844z" p-id="2414" fill="#FE672E"></path><path d="M933.722904 236.364289 88.354304 236.364289c-13.508665 0-24.461111 10.952446-24.461111 24.461111L63.893192 768.045537c0 13.508665 10.952446 24.461111 24.461111 24.461111l845.367577 0c13.508665 0 24.461111-10.952446 24.461111-24.461111L958.182992 260.824377C958.182992 247.315712 947.230546 236.364289 933.722904 236.364289zM909.261793 743.584426 112.815415 743.584426 112.815415 285.285488l796.446377 0L909.261793 743.584426z" p-id="2415" fill="#FE672E"></path></svg>'+
-                     '<span class="singer">'+i.singer+'</span>'+
-                 '</p>'+
-                  '<a href="./play/play.html?id='+i.id+'">'+
-                     '<svg t="1504492168101" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2380" xmlns:xlink="http://www.w3.org/1999/xlink" width="6vw" height="6vw"><defs></defs><path d="M512 0C229.23 0 0 229.23 0 512c0 282.77 229.23 512 512 512s512-229.23 512-512C1024 229.23 794.771 0 512 0zM512 960C264.577 960 64 759.424 64 512S264.577 64 512 64s448 200.576 448 448S759.424 960 512 960z" p-id="2381" fill="#888888"></path><path d="M384 768 768.826 512 384 256Z" p-id="2382" fill="#888888"></path></svg>'+
-                '</a>'+
-                '</li>'
+        var item = response
+        item.forEach(function(i){
+            var $li='<li>'+
+                     '<h3>'+i.name+'<span>'+i.description+'</span></h3>'+
+                     '<p>'+
+                         '<img src="./images/sq.png" alt="" class="sq">'+
+                         '<span class="singer">'+i.singer+'</span>'+
+                     '</p>'+
+                      '<a href="./play/play.html?id='+i.id+'">'+
+                         '<img src="./images/play.png" alt="" style="height:5vw;width:5vw">'+
+                    '</a>'+
+                    '</li>'
             $('#latestmusicList').append($($li))
         })
         $('.loading').remove()
@@ -22,14 +22,14 @@ $(function () {
 
     //选项卡切换
     $('.sitenav').on('click', '.tabitem>li', function (e) {
-        let $li = $(e.currentTarget)
-        let index = $li.index()
+        var $li = $(e.currentTarget)
+        var index = $li.index()
         $li.trigger('tabChange', index)
         $li.addClass('active').siblings().removeClass('active')
     })
 
     $('.sitenav').on('tabChange', function (e, index) {
-        let $contentlis = $('.content>li')
+        var $contentlis = $('.content>li')
         $contentlis.eq(index).addClass('active').siblings().removeClass('active')
 
         //热歌榜请求
@@ -39,28 +39,28 @@ $(function () {
                  return 
             }
             $.get('./tab2.json').then(function (result) {
-                let item = result;
-                let $bottomMore = $(`<div class="fulllist">
-                <span>查看完整榜单</span>
-                </div>`)
-                item.forEach((i) => {
-                    let $li='<li>'+
+                var item = result;
+                var $bottomMore = '<div class="fulllist">'+
+                '<span>查看完整榜单</span>'+
+                '</div>'
+                item.forEach(function(i){
+                    var $li='<li>'+
                     '<div class="songindex">'+i.id+'</div>'+
                     '<div class="song-content">'+
                         '<h3>'+i.name+'<span>'+i.description+'</span></h3>'+
                         '<p>'+
-                            '<svg t="1504499575221" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2412" xmlns:xlink="http://www.w3.org/1999/xlink" width="4.2vw" height="6vw"><defs></defs><path d="M537.456788 684.682921l198.722994 0 18.48398 18.023492c5.709025 5.565762 13.102413 8.336876 20.490683 8.336876 7.636934 0 15.266705-2.962471 21.018709-8.859785 11.317767-11.607362 11.083429-30.191626-0.522909-41.509393l-17.499559-17.063631L778.150686 373.540532c0-16.210193-13.143345-29.352515-29.353538-29.352515L537.456788 344.188017c-16.210193 0-29.352515 13.143345-29.352515 29.352515l0 281.788851C508.104273 671.539576 521.246595 684.682921 537.456788 684.682921zM566.810327 402.893047l152.634306 0L719.444633 586.367755l-2.808976-2.739391c-11.611455-11.317767-30.193673-11.081383-41.509393 0.522909-11.317767 11.607362-11.083429 30.191626 0.522909 41.509393l0.323365 0.315178L566.810327 625.975844 566.810327 402.893047z" p-id="2413" fill="#FE672E"></path><path d="M220.442668 625.976868c-16.210193 0-29.352515 13.143345-29.352515 29.353538s13.143345 29.352515 29.352515 29.352515l211.342406 0c16.210193 0 29.352515-13.143345 29.352515-29.352515L461.137589 514.433422c0-16.210193-13.143345-29.353538-29.352515-29.353538L249.796206 485.079884l0-82.187861 181.989891 0c16.210193 0 29.352515-13.143345 29.352515-29.352515 0-16.210193-13.143345-29.352515-29.352515-29.352515L220.442668 344.186993c-16.210193 0-29.352515 13.143345-29.352515 29.352515l0 140.893914c0 16.210193 13.143345 29.352515 29.352515 29.352515l181.989891 0 0 82.189907L220.442668 625.975844z" p-id="2414" fill="#FE672E"></path><path d="M933.722904 236.364289 88.354304 236.364289c-13.508665 0-24.461111 10.952446-24.461111 24.461111L63.893192 768.045537c0 13.508665 10.952446 24.461111 24.461111 24.461111l845.367577 0c13.508665 0 24.461111-10.952446 24.461111-24.461111L958.182992 260.824377C958.182992 247.315712 947.230546 236.364289 933.722904 236.364289zM909.261793 743.584426 112.815415 743.584426 112.815415 285.285488l796.446377 0L909.261793 743.584426z" p-id="2415" fill="#FE672E"></path></svg>'+
+                        '<img src="./images/sq.png" alt="" class="sq">'+
                             '<span class="singer">'+i.singer+'</span>'+
                         '</p>'+
                     '</div>'+
                     '<a href="./play/play.html?id='+i.id+'">'+
-                        '<svg t="1504492168101" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2380" xmlns:xlink="http://www.w3.org/1999/xlink" width="6vw" height="6vw"><defs></defs><path d="M512 0C229.23 0 0 229.23 0 512c0 282.77 229.23 512 512 512s512-229.23 512-512C1024 229.23 794.771 0 512 0zM512 960C264.577 960 64 759.424 64 512S264.577 64 512 64s448 200.576 448 448S759.424 960 512 960z" p-id="2381" fill="#888888"></path><path d="M384 768 768.826 512 384 256Z" p-id="2382" fill="#888888"></path></svg>'+
+                        '<img src="./images/play.png" alt="" style="height:5vw;width:5vw">'+
                     '</a>'+
                     '</li>'
                     $('.hotSongList>.songList').append($($li))
                     $contentlis.eq(index).attr('data-downloaded', 'yes')
                 })
-                $('.hotSongList>.songList').append($bottomMore)
+                $('.hotSongList>.songList').append($($bottomMore))
                 $('.hotSongList>.contentLoading').remove()
             }, function (err) {
                 console.log('出错了:' + err)
@@ -70,12 +70,12 @@ $(function () {
 })
 
 
-//搜索页面逻辑
+// 搜索页面逻辑
 $(function () {
-    let timer = null;
-    let $output=$('.output')
-    let $iconback=$('.icon-back')
-    let $hotsearch=$('.hot-search')
+    var timer = null;
+    var $output=$('.output')
+    var $iconback=$('.icon-back')
+    var $hotsearch=$('.hot-search')
     $iconback.on('click', function () {
         $('#search').val('')
         $output.text('')
@@ -85,7 +85,7 @@ $(function () {
     })
 
     $('.hot-search>.list').on('click','li',function(){
-        let id=$(this).attr('data-id')
+        var id=$(this).attr('data-id')
         $hotsearch.removeClass('active')
         $iconback.addClass('active')
         sendRequest(+id) //string=>number
@@ -95,7 +95,7 @@ $(function () {
         $hotsearch.removeClass('active')
         $iconback.addClass('active')
         $('.searchSong>.songList').empty()
-        let value = $(e.currentTarget).val().trim()
+        var value = $(e.currentTarget).val().trim()
         if (timer) {
             clearTimeout(timer) //如果300ms内再次输入则清除之前的定时器,重新生成
         }
@@ -106,16 +106,16 @@ $(function () {
 
     function startSearch(value){
         timer = setTimeout(function () {
-            search(value).then((result) => {
+            search(value).then(function(result){
                 if (result.length !== 0) {
-                    $output.html(result.map(item => { return `<h3>${item.name }</he>`}))
-                    let h3Nodes=document.querySelectorAll('.output>h3')
+                    $output.html(result.map(function(item){ return '<h3>'+item.name+'</h3>'}))
+                    var h3Nodes=document.querySelectorAll('.output>h3')
                     $output[0].onclick=function(e){   //用原生js方法多次绑定事件
-                        let $loading = '<img class="contentLoading" src="./images/loading.gif" alt="">'
+                        var $loading = '<img class="contentLoading" src="./images/loading.gif" alt="">'
                         $output.text('')
                         $('.searchSong').append($loading)
-                        let index=[].indexOf.call(h3Nodes,e.target)  //区分多个搜索结果
-                        let { id } = result[index]
+                        var index=[].indexOf.call(h3Nodes,e.target)  //区分多个搜索结果
+                        var id  = result[index].id
                         sendRequest(id)
                     }
                 } else {
@@ -127,50 +127,50 @@ $(function () {
     }
 
     function search(keyword) {
-        return new Promise((resolve, reject) => {
+        return new Promise(function(resolve, reject){
             var database = [
                 {
-                    "id": 1,
+                    "id": "01",
                     "name": "Ready For It?"
                 },
                 {
-                    "id": 2,
+                    "id": "02",
                     "name": "我在夜里偷看过一颗星星"
                 },
                 {
-                    "id": 3,
+                    "id": "03",
                     "name": "SHE"
                 },
                 {
-                    "id": 4,
+                    "id": "04",
                     "name": "普通人"
                 },
                 {
-                    "id": 5,
+                    "id": "05",
                     "name": "Baby U Know"
                 },
                 {
-                    "id": 6,
+                    "id": "06",
                     "name": "英雄归来"
                 },
                 {
-                    "id": 7,
+                    "id": "07",
                     "name": "谁比我有范儿 (舒服版)"
                 },
                 {
-                    "id": 8,
+                    "id": "08",
                     "name": "哭给你听"
                 },
                 {
-                    "id": 9,
+                    "id": "09",
                     "name": "云长"
                 },
                 {
-                    "id": 10,
+                    "id": "10",
                     "name": "当冬夜渐暖"
                 }
             ]
-            let result = database.filter(function (item) {
+            var result = database.filter(function (item) {
                 return item.name.indexOf(keyword) >= 0 || item.name.indexOf(keyword.toUpperCase()) >= 0
             })
             setTimeout(function () {
@@ -182,17 +182,18 @@ $(function () {
 
     function sendRequest(id){
         $.get('./tab2.json').then(function (response) {
-            let searchResult = response.filter(i => i.id === id)
-            let $li = '<li>'+
+            var searchResult = response.filter(function(i){return i.id == id})
+            console.log(searchResult)
+            var $li = '<li>'+
             '<div class="song-content"'+
                 '<h3>'+searchResult[0].name+'<span>'+searchResult[0].description+'</span></h3>'+
                 '<p>'+
-                    '<svg t="1504499575221" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2412" xmlns:xlink="http://www.w3.org/1999/xlink" width="4.2vw" height="6vw"><defs></defs><path d="M537.456788 684.682921l198.722994 0 18.48398 18.023492c5.709025 5.565762 13.102413 8.336876 20.490683 8.336876 7.636934 0 15.266705-2.962471 21.018709-8.859785 11.317767-11.607362 11.083429-30.191626-0.522909-41.509393l-17.499559-17.063631L778.150686 373.540532c0-16.210193-13.143345-29.352515-29.353538-29.352515L537.456788 344.188017c-16.210193 0-29.352515 13.143345-29.352515 29.352515l0 281.788851C508.104273 671.539576 521.246595 684.682921 537.456788 684.682921zM566.810327 402.893047l152.634306 0L719.444633 586.367755l-2.808976-2.739391c-11.611455-11.317767-30.193673-11.081383-41.509393 0.522909-11.317767 11.607362-11.083429 30.191626 0.522909 41.509393l0.323365 0.315178L566.810327 625.975844 566.810327 402.893047z" p-id="2413" fill="#FE672E"></path><path d="M220.442668 625.976868c-16.210193 0-29.352515 13.143345-29.352515 29.353538s13.143345 29.352515 29.352515 29.352515l211.342406 0c16.210193 0 29.352515-13.143345 29.352515-29.352515L461.137589 514.433422c0-16.210193-13.143345-29.353538-29.352515-29.353538L249.796206 485.079884l0-82.187861 181.989891 0c16.210193 0 29.352515-13.143345 29.352515-29.352515 0-16.210193-13.143345-29.352515-29.352515-29.352515L220.442668 344.186993c-16.210193 0-29.352515 13.143345-29.352515 29.352515l0 140.893914c0 16.210193 13.143345 29.352515 29.352515 29.352515l181.989891 0 0 82.189907L220.442668 625.975844z" p-id="2414" fill="#FE672E"></path><path d="M933.722904 236.364289 88.354304 236.364289c-13.508665 0-24.461111 10.952446-24.461111 24.461111L63.893192 768.045537c0 13.508665 10.952446 24.461111 24.461111 24.461111l845.367577 0c13.508665 0 24.461111-10.952446 24.461111-24.461111L958.182992 260.824377C958.182992 247.315712 947.230546 236.364289 933.722904 236.364289zM909.261793 743.584426 112.815415 743.584426 112.815415 285.285488l796.446377 0L909.261793 743.584426z" p-id="2415" fill="#FE672E"></path></svg>'+
+                    '<img src="./images/play.png" alt="" style="height:5vw;width:5vw">'+
                     '<span class="singer">'+searchResult[0].singer+'</span>'+
                 '</p>'+
             '</div>'+
             '<a href="./play/play.html?id='+searchResult[0].id+'">'+
-                '<svg t="1504492168101" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2380" xmlns:xlink="http://www.w3.org/1999/xlink" width="6vw" height="6vw"><defs></defs><path d="M512 0C229.23 0 0 229.23 0 512c0 282.77 229.23 512 512 512s512-229.23 512-512C1024 229.23 794.771 0 512 0zM512 960C264.577 960 64 759.424 64 512S264.577 64 512 64s448 200.576 448 448S759.424 960 512 960z" p-id="2381" fill="#888888"></path><path d="M384 768 768.826 512 384 256Z" p-id="2382" fill="#888888"></path></svg>'+
+                '<img src="./images/play.png" alt="" style="height:5vw;width:5vw">'+
             '</a>'+
             '</li>'
             $('.searchSong>.contentLoading').remove()
